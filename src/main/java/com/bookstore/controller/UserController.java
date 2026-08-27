67a30a848c53ad3e041cac421839ac73201a1488
Khishigbayar <hishgee.kira@gmail.com>
2026-08-27 16:57:40 +0800

    bookstore user finished

---
+++ src/main/java/com/bookstore/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.bookstore.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.bookstore.dto.*;
+import com.bookstore.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+	private final UserService userService;
+	
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping
+	public List<UserResponse> findAll() {
+		return userService.findAllUsers();
+	}
+	
+	@PostMapping
+	public UserResponse create(@RequestBody UserCreateRequest request) {
+		return userService.createUser(request);
+	}
+	
+}
